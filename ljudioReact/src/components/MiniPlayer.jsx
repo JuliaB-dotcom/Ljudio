8b7db8b4b5d6a1556a8b26fcf723539705fdf04e
JuliB-dotcom <69867011+JuliaB-dotcom@users.noreply.github.com>
2021-09-28 20:13:47 +0200

    Fixed css on buttons

--- ljudioReact/src/components/MiniPlayer.jsx
+++ ljudioReact/src/components/MiniPlayer.jsx
@@ -3,28 +3,32 @@ import '/src/MiniPlayer.css'
 function MiniPlayer() {
   return (
     <div>
-    <div className="box border">
+
         <p className="songName">Songname</p>
-        <div className="playerBtns">
+       
+          <div className="prevBtns">
         <svg className="prevBtn1" width="26" height="25" viewBox="0 0 26 25" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M24.4671 23.7391L1.1976 13.079L22.7149 1.29243L24.4671 23.7391Z" fill="white" stroke="#350E88" />
         </svg>
         <svg className="prevBtn2" width="26" height="25" viewBox="0 0 26 25" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M24.4671 23.7391L1.1976 13.079L22.7149 1.29243L24.4671 23.7391Z" fill="white" stroke="#350E88" />
-        </svg>
+            </svg>
+      </div>
+      <div className="play">
         <svg className="playBtn" width="36" height="25" viewBox="0 0 36 25" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M1.01459 1.18008L33.5442 12.4763L1.10814 23.8588L1.01459 1.18008Z" fill="white" stroke="#350E88" />
         </svg>
+      </div>
+          <div className="nextBtns">
         <svg className="nextBtn1" width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M0.523226 1.71076L22.9756 12.9085L0.60002 24.1781L0.523226 1.71076Z" fill="white" stroke="#350E88" />
         </svg>
         <svg className="nextBtn2" width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M0.525107 1.29047L22.9854 12.4943L0.617789 23.7577L0.525107 1.29047Z" fill="white" stroke="#350E88" />
-        </svg>
-        </div>
-
-
-      </div>
+            </svg>
+            </div>
+    
+  
      
   </div>
 )
