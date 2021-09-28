c8d6dfff114f5ded8fd34e47650cafedaf686382
Sebastian Kähkönen <seebbe_@hotmail.com>
2021-09-28 23:25:52 +0200

    Worked with CSS and JSX, Added buttons for queue and playlist, added songname and a results div

--- ljudioReact/src/components/Search.jsx
+++ ljudioReact/src/components/Search.jsx
@@ -4,7 +4,42 @@ import '/src/Search.css'
 function Search() {
   return (
     <div>
-      <input type="text" className="searchBar" placeholder="Search" />
+      <div className="searchBar">
+        <input type="text" className="inputField" placeholder="Search" />
+        <div className="searchResultsDiv">
+          <div className="addToButtons">
+            <a href="#">This is the greatest song</a>
+            <button className="queueButton">Queue</button>
+            <button className="playlistButton">Playlist</button>
+          </div>
+          <div className="addToButtons">
+            <a href="#">Wow what a song this is</a>
+            <button className="queueButton">Queue</button>
+            <button className="playlistButton">Playlist</button>
+          </div>
+          <div className="addToButtons">
+            <a href="#">Sing along to my song</a>
+            <button className="queueButton">Queue</button>
+            <button className="playlistButton">Playlist</button>
+          </div>
+          <div className="addToButtons">
+            <a href="#">No ideas at all song</a>
+            <button className="queueButton">Queue</button>
+            <button className="playlistButton">Playlist</button>
+          </div>
+          <div className="addToButtons">
+            <a href="#">lorem ipsum song wow</a>
+            <button className="queueButton">Queue</button>
+            <button className="playlistButton">Playlist</button>
+          </div>
+          <div className="addToButtons">
+            <a href="#">crazy frog song is great</a>
+            <button className="queueButton">Queue</button>
+            <button className="playlistButton">Playlist</button>
+          </div>
+        </div>
+
+      </div>
     </div>
   )
 }
