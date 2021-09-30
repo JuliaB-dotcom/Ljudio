3129add645293ed1c55d862ae4d1b1844060621f
Sena A.H <sena@Senas-Air.lan>
2021-09-30 16:19:47 +0200

    changed video

--- ljudioReact/src/components/BigPlayer.jsx
+++ ljudioReact/src/components/BigPlayer.jsx
@@ -6,7 +6,8 @@ function BigPlayer() {
 
     <div className="bigPlayerDiv">
       <div className="videoDiv">
-        <iframe className="video" width="560" height="315" src="https://www.youtube.com/embed/1QD0FeZyDtQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>      </div>
+        <iframe className="video" width="560" height="315" src="https://www.youtube.com/embed/mGeOehe3jIU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
       <div className="btnDiv">
 
         <button type="button" className="shuffle">shuffle</button>
