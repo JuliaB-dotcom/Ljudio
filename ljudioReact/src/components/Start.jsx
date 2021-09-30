a116e12545c6865e0cdd04e32606ff96af9561b0
Sebastian Kähkönen <seebbe_@hotmail.com>
2021-09-30 13:13:08 +0200

    Prototype PNG button

--- ljudioReact/src/components/Start.jsx
+++ ljudioReact/src/components/Start.jsx
@@ -1,13 +1,18 @@
 import '/src/Start.css'
 
 function Start() {
-  
+  function test() {
+    console.log("hey");
+  }
   return (
     <div>
       <h1 className="title">Ljudio</h1>
-       <h1 className="title2">Ljudio</h1>
+      <h1 className="title2">Ljudio</h1>
+
+      <button className="play" onClick={test}></button>
+
     </div>
-   
+
   )
 }
 
