2c8fcad7ea57526043daa55147d62cd103eb789f
Sena A.H <sena@Senas-Air.lan>
2021-09-30 15:57:19 +0200

    resized video and placed buttons under video

--- ljudioReact/src/components/BigPlayer.jsx
+++ ljudioReact/src/components/BigPlayer.jsx
@@ -4,9 +4,10 @@ function BigPlayer() {
 
   return (
 
-    <div>
-      <iframe className="video" width="560" height="315" src="https://www.youtube.com/embed/cO1cx9oXp1k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
+    <div className="bigPlayerDiv">
+      <div className="videoDiv">
+        <iframe className="video" width="560" height="315" src="https://www.youtube.com/embed/cO1cx9oXp1k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
       <div className="btnDiv">
 
         <button type="button" className="shuffle">shuffle</button>
