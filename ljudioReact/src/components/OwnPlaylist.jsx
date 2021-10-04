1a852c6e90a65b413a59accaadaef022470c6793
JuliB-dotcom <69867011+JuliaB-dotcom@users.noreply.github.com>
2021-10-04 11:41:01 +0200

    Updated css

--- ljudioReact/src/components/OwnPlaylist.jsx
+++ ljudioReact/src/components/OwnPlaylist.jsx
@@ -3,7 +3,16 @@ import "/src/OwnPlaylist.css"
 
 function OwnPlaylist() {
   return (
-    <div></div>
+    <div>
+      <div className="playlistDiv">
+        <div className="playlistNameDiv">
+        <button className="playlistNameBtn"> PlAYLIST NAME</button>
+        </div>
+      <div className="saveBtnDiv">
+        <button className="saveBtn">SAVE</button>
+      </div>
+    </div>
+    </div>
   )
 }
 
