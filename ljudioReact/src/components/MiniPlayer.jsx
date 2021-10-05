fe5b8236910b92affd7e46fbb01cb8ef4dfd33c3
Sebastian Kähkönen <seebbe_@hotmail.com>
2021-10-05 11:03:24 +0200

    Code-along, MINIPLAYER and SEARCH merged together

--- ljudioReact/src/components/MiniPlayer.jsx
+++ ljudioReact/src/components/MiniPlayer.jsx
@@ -2,26 +2,24 @@
 import '/src/MiniPlayer.css'
 function MiniPlayer() {
   return (
-    <div>
+    <div className="parent">
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <div className="parent">
-        <div className="songDiv">
+
+      <div className="songDiv">
         <h1 className="songName">Songname</h1>
-        </div>
+      </div>
       <div className="playItems">
-      <button className="prevButt" onclick="{}"></button>
-      <button className="playButt" onclick="{}"></button>
-          <button className="nextButt" onclick="{}"></button>
-          </div>
-          
-        <div className="textButtons">
-          <button className="playlist" onclick="{}">PLAYLIST</button>
-          <button className="playlist" onclick="{}">Queu</button>
-          <button className="playlist" onclick="{}">RANDOM</button>
-        </div>
-      
-      </div>  
-  </div>
-)
+        <button className="prevButt" onclick="{}"></button>
+        <button className="playButt" onclick="{}"></button>
+        <button className="nextButt" onclick="{}"></button>
+      </div>
+
+      <div className="textButtons">
+        <button className="playlist" onclick="{}">PLAYLIST</button>
+        <button className="playlist" onclick="{}">Queu</button>
+        <button className="playlist" onclick="{}">RANDOM</button>
+      </div>
+    </div>
+  )
 }
 export default MiniPlayer
