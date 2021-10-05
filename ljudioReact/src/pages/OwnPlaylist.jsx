3662b237499b2a724ba344da6d6238c6c0863a57
JuliB-dotcom <69867011+JuliaB-dotcom@users.noreply.github.com>
2021-10-05 21:03:41 +0200

    Updated Css on OwnPlaylist

--- ljudioReact/src/pages/OwnPlaylist.jsx
+++ ljudioReact/src/pages/OwnPlaylist.jsx
@@ -3,34 +3,46 @@ import "/src/CSS/OwnPlaylist.css"
 
 function OwnPlaylist() {
   return (
-    <div>
-      <div className="playlistDiv">
+    <div className="OwnPlaylistDiv">
+      <div className="playlistDiv1">
         <div className="playlistNameDiv">
-          <button> PlAYLIST NAME</button>
-        </div>
+          <button className="playListNameBtn"> PLAYLIST NAME</button>
+      </div>
+
         <div className="saveBtnDiv">
           <button>SAVE</button>
         </div>
 
         <div className="songDiv1">
           <p>Låt1</p>
-        </div>
-        <div className="changeBtns1">
           <button className="queueSong">+</button>
           <button className="removeSong">x</button>
         </div>
         {/* <button>-</button> */}
 
-
-        <div className="songDiv2">
+        <div className="songDiv1">
           <p>Låt2</p>
-        </div>
-        <div className="changeBtns2">
           <button className="queueSong">+</button>
           <button className="removeSong">x</button>
         </div>
+    
         {/* <button>-</button> */}
 
+        <div className="songDiv1">
+          <p>Låt3</p>
+          <button className="queueSong">+</button>
+          <button className="removeSong">x</button>
+        </div>
+
+        {/* <button>-</button> */}
+
+        <div className="songDiv1">
+          <p>Låt4</p>
+          <button className="queueSong">+</button>
+          <button className="removeSong">x</button>
+        </div>
+
+        {/* <button>-</button> */}
       </div>
     </div>
   )
