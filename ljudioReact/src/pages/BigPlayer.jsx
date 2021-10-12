ac592f0cedab1cabec5e312b1c9e4dbb1401161f
JuliB-dotcom <69867011+JuliaB-dotcom@users.noreply.github.com>
2021-10-12 19:40:35 +0200

    Changed count on newPosition and it might work

--- ljudioReact/src/pages/BigPlayer.jsx
+++ ljudioReact/src/pages/BigPlayer.jsx
@@ -14,7 +14,7 @@ function BigPlayer() {
     setInterval(() => {
       let currentTime = context.player.getCurrentTime()
       let duration = context.player.getDuration()
-      let playedPercent = (currentTime / duration) * 100
+      let playedPercent = currentTime * (100 / duration) 
 
       // TODO: don't update when user is moving the slider
 
@@ -25,7 +25,7 @@ function BigPlayer() {
   function changeSongPosition(e) {
     setProgress(e.target.value)
 
- let newPosition = context.player.getDuration() / e.target.value
+ let newPosition = context.player.getDuration() * (e.target.value/100)
     console.log(newPosition)
     // change position in song
     context.player.seekTo(newPosition, true)
