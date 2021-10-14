fce91f037548a82d72c6b6a5dc87386877e18565
Sebastian Kähkönen <seebbe_@hotmail.com>
2021-10-14 10:40:05 +0200

    Revert "Code along, some code for tomorrow"
    This reverts commit eeb73a4edad6504c7b0ea51533150fac35959e34.

--- ljudioReact/src/components/Player.jsx
+++ ljudioReact/src/components/Player.jsx
@@ -3,7 +3,7 @@ import { PlayerContext } from '/src/contexts/PlayerContexts'
 import '/src/CSS/Player.css'
 import { useHistory } from "react-router-dom"
 import Search from "./Search"
-import Youtube from 'react-youtube'
+
 
 // props with a function to update song
 function Player({ videoId }) {
@@ -30,137 +30,91 @@ function Player({ videoId }) {
 
   function loadPlayer() {
     let ytPlayer = new YT.Player('yt-player', {
-      < Youtube
-
       height: '0px',
       width: '0px',
       events: {
-      'onStateChange': onPlayerStateChange,
-      'onReady': (e) => {
-        e.target.setVolume(10)
-        setPlayer(e.target)
-        updateContext({
-          player: e.target
-        }),
+        'onStateChange': onPlayerStateChange,
+        'onReady': (e) => {
+          e.target.setVolume(10)
+          setPlayer(e.target)
+          updateContext({
+            player: e.target
+          }),
 
-          getdurationandtime
-      },
-      'onEnd': (e) => {
-        playNextSongAutomatically();
+            getdurationandtime
+        }
       }
-    }
     });
-  />
-}
-
-function playNextSongAutomatically() {
-  if (queueSongs.length) {
-    autoPlayNextInQueue();
-  } else if (playList.length) {
-    autoPlayNextInPlayList();
   }
-}
-
-function autoPlayNext() {
-  let song = context.queue[0];
-
-  ytPlayer.player.loadVideoById(song.videoId);
-  ytPlayer.player.playVideo();
-  setCurrentSong(song);
-}
-//Shuffle attempt 1
-// let getRandom = function (min, max) {
-//   return Math.random() * (max - min) + min;
-// }
-
-// function shuffle() {
-
-//     let num = getRandom(0, 99);
-
-//     player.playVideoAt(num);
-//   }
-
-// this function triggers when we change song in player
-// can be used to update things, like counters
-
-
-
-// function onPlayerPlaying(event) {
-
-//   if (event.data === YT.PlayerState.PLAYING) {
-
-//     let interval = setInterval(() => {
-
-//       let currentTime = context.player.getCurrentTime()
-//       let duration = context.player.getDuration()
-//       let playedPercent = currentTime * (100 / duration)
-
-
-//       if (playedPercent > 98) {
-//         console.log("halååå!", playedPercent);
-//         nextSong();
-
-//         clearInterval(interval);
-//       }
-//       context.player.setPlayer(interval);
-//     }, 1000)
-
-
-//   }
-// }
-
-function onPlayerStateChange(event) {
-  // if (event.data !== YT.PlayerState.PLAYING) {
-
-  //   let interval = setInterval(() => {
-
-  //     let currentTime = context.player.getCurrentTime()
-  //     let duration = context.player.getDuration()
-  //     let playedPercent = currentTime * (100 / duration)
-
-
-  //     if (playedPercent > 98) {
-  //       console.log("halååå!", playedPercent);
-  //       nextSong();
-
-  //       clearInterval(interval);
-  //     }
-
-  //   }, 1000)
-
-
-}
-
-
-
-
-function getdurationandtime() {
-  // if (context.player.getCurrentTime() > (context.player.getDuration() - 10)) {
+  //Shuffle attempt 1
+  // let getRandom = function (min, max) {
+  //   return Math.random() * (max - min) + min;
   // }
 
-  let interval = setInterval(() => {
+  // function shuffle() {
 
-    let currentTime = context.player.getCurrentTime()
-    let duration = context.player.getDuration()
-    let playedPercent = currentTime * (100 / duration)
+  //     let num = getRandom(0, 99);
 
-    if (playedPercent > 90) {
-      console.log("halååå!", playedPercent);
-      nextSong();
-      // document.getElementById('nextBtn').click();
-      clearInterval(interval);
-    }
+  //     player.playVideoAt(num);
+  //   }
 
-  }, 1000)
+  // this function triggers when we change song in player
+  // can be used to update things, like counters
 
 
-  console.log("getduration: ", context.player.getDuration())
-  console.log("getCurrentTime: ", context.player.getCurrentTime())
-}
 
-function playSong() {
-  console.log(player.pauseVideo());
-  if (context.currentSong !== null) {
+  // function onPlayerPlaying(event) {
+
+  //   if (event.data === YT.PlayerState.PLAYING) {
+
+  //     let interval = setInterval(() => {
+
+  //       let currentTime = context.player.getCurrentTime()
+  //       let duration = context.player.getDuration()
+  //       let playedPercent = currentTime * (100 / duration)
+
+
+  //       if (playedPercent > 98) {
+  //         console.log("halååå!", playedPercent);
+  //         nextSong();
+
+  //         clearInterval(interval);
+  //       }
+  //       context.player.setPlayer(interval);
+  //     }, 1000)
+
+
+  //   }
+  // }
+
+  function onPlayerStateChange(event) {
+    // if (event.data !== YT.PlayerState.PLAYING) {
+
+    //   let interval = setInterval(() => {
+
+    //     let currentTime = context.player.getCurrentTime()
+    //     let duration = context.player.getDuration()
+    //     let playedPercent = currentTime * (100 / duration)
+
+
+    //     if (playedPercent > 98) {
+    //       console.log("halååå!", playedPercent);
+    //       nextSong();
+
+    //       clearInterval(interval);
+    //     }
+
+    //   }, 1000)
+
+
+  }
+
+
+
+
+  function getdurationandtime() {
+    // if (context.player.getCurrentTime() > (context.player.getDuration() - 10)) {
+    // }
 
     let interval = setInterval(() => {
 
@@ -168,135 +122,159 @@ function playSong() {
       let duration = context.player.getDuration()
       let playedPercent = currentTime * (100 / duration)
 
-
-      if (playedPercent > 99) {
+      if (playedPercent > 90) {
         console.log("halååå!", playedPercent);
         nextSong();
-
+        // document.getElementById('nextBtn').click();
         clearInterval(interval);
       }
 
     }, 1000)
 
-    player.playVideo();
+
+    console.log("getduration: ", context.player.getDuration())
+    console.log("getCurrentTime: ", context.player.getCurrentTime())
   }
-}
 
-function pauseSong() {
-  console.log(player.pauseVideo());
-  console.log(player.playVideo());
-  player.pauseVideo();
-}
+  function playSong() {
+    console.log(player.pauseVideo());
+    if (context.currentSong !== null) {
 
-function previousSong() {
-  let songPlaying = context.currentSong;
-  console.log("currentSong", context.currentSong);
-  console.log("QueueArray", context.queue)
-  let index = context.queue.indexOf(songPlaying);
-  console.log(index);
-  let addIndex = index - 1;
-  let newSong = context.queue[addIndex];
-  console.log(newSong);
+      let interval = setInterval(() => {
 
-  context.player.loadVideoById(newSong.videoId)
-  updateContext({ currentSong: newSong });
-  setCurrentVideoId(newSong.videoId);
-}
-
-function nextSong() {
-  console.log("This is playing: ", context.currentSong);
-  let songPlaying = context.currentSong;
-  console.log("currentSong", context.currentSong);
-  console.log("QueueArray", context.queue)
-  let index = context.queue.indexOf(songPlaying);
-  console.log(index);
-  let addIndex = index + 1;
-  let newSong = context.queue[addIndex];
-  console.log(newSong);
-
-  context.player.loadVideoById(newSong.videoId)
-  updateContext({ currentSong: newSong });
-  setCurrentVideoId(newSong.videoId);
-}
-
-function autoNext() {
-
-  console.log("autoNext function helloo");
-
-  let index = context.queue.indexOf();
+        let currentTime = context.player.getCurrentTime()
+        let duration = context.player.getDuration()
+        let playedPercent = currentTime * (100 / duration)
 
 
-}
-// function queueLoop(queue) {
+        if (playedPercent > 99) {
+          console.log("halååå!", playedPercent);
+          nextSong();
+
+          clearInterval(interval);
+        }
+
+      }, 1000)
+
+      player.playVideo();
+    }
+  }
+
+  function pauseSong() {
+    console.log(player.pauseVideo());
+    console.log(player.playVideo());
+    player.pauseVideo();
+  }
+
+  function previousSong() {
+    let songPlaying = context.currentSong;
+    console.log("currentSong", context.currentSong);
+    console.log("QueueArray", context.queue)
+    let index = context.queue.indexOf(songPlaying);
+    console.log(index);
+    let addIndex = index - 1;
+    let newSong = context.queue[addIndex];
+    console.log(newSong);
+
+    context.player.loadVideoById(newSong.videoId)
+    updateContext({ currentSong: newSong });
+    setCurrentVideoId(newSong.videoId);
+  }
+
+  function nextSong() {
+    console.log("This is playing: ", context.currentSong);
+    let songPlaying = context.currentSong;
+    console.log("currentSong", context.currentSong);
+    console.log("QueueArray", context.queue)
+    let index = context.queue.indexOf(songPlaying);
+    console.log(index);
+    let addIndex = index + 1;
+    let newSong = context.queue[addIndex];
+    console.log(newSong);
+
+    context.player.loadVideoById(newSong.videoId)
+    updateContext({ currentSong: newSong });
+    setCurrentVideoId(newSong.videoId);
+  }
+
+  function autoNext() {
+
+    console.log("autoNext function helloo");
+
+    let index = context.queue.indexOf();
 
 
-//   queue.array.forEach(() => { console.log("hej forEach") }
-
-//   );
-
-// }
+  }
+  // function queueLoop(queue) {
 
 
+  //   queue.array.forEach(() => { console.log("hej forEach") }
 
-return (
+  //   );
+
+  // }
 
 
-  <div>
 
-    <div id="yt-player">
+  return (
+
+
+    <div>
+
+      <div id="yt-player">
+
+      </div>
+
+
+      <div className="parent">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <div className="songDiv123">
+          <h1 className="songName" onClick={() => { history.push('/bigplayer') }}>{context.currentSong.name}</h1>
+          <button onClick={getdurationandtime}>CLICK HERE</button>
+        </div>
+
+        <div className="playItems">
+
+          <button type="button" className="shuffleButt">shuffle</button>
+
+          <button className="prevButt" onClick={previousSong}></button>
+          <button className="pauseButt" onClick={pauseSong}></button>
+          <button className="playButt" onClick={playSong}></button>
+          <button id="nextBtn" className="nextButt" onClick={nextSong}></button>
+
+          <button type="button" className="loopButt">Loop</button>
+        </div>
+
+
+
+        <div className="playItems">
+
+
+
+        </div>
+
+
+
+        <div className="textButtons">
+
+          <button className="playlist" onClick={() => { history.push('/playlist') }}>PLAYLIST</button>
+
+          <button className="playlist" onClick={() => { history.push('/queue') }}>Queue</button>
+
+          <button className="playlist">RANDOM</button>
+
+        </div>
+
+      </div>
+
+
+
 
     </div>
 
-
-    <div className="parent">
-
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-      <div className="songDiv123">
-        <h1 className="songName" onClick={() => { history.push('/bigplayer') }}>{context.currentSong.name}</h1>
-        <button onClick={getdurationandtime}>CLICK HERE</button>
-      </div>
-
-      <div className="playItems">
-
-        <button type="button" className="shuffleButt">shuffle</button>
-
-        <button className="prevButt" onClick={previousSong}></button>
-        <button className="pauseButt" onClick={pauseSong}></button>
-        <button className="playButt" onClick={playSong}></button>
-        <button id="nextBtn" className="nextButt" onClick={nextSong}></button>
-
-        <button type="button" className="loopButt">Loop</button>
-      </div>
-
-
-
-      <div className="playItems">
-
-
-
-      </div>
-
-
-
-      <div className="textButtons">
-
-        <button className="playlist" onClick={() => { history.push('/playlist') }}>PLAYLIST</button>
-
-        <button className="playlist" onClick={() => { history.push('/queue') }}>Queue</button>
-
-        <button className="playlist">RANDOM</button>
-
-      </div>
-
-    </div>
-
-
-
-
-  </div>
-
-)
+  )
 }
 
 export default Player
